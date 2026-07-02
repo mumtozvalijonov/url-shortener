9fb601b1f61cc39f175472eafef71ca7ce92168e
mumtozvalijonov <mumtozvalijonov@gmail.com>
2026-07-02 22:45:58 +0200

    refactor: construct HandlerFunc within Handler's functions

--- backend/internal/adapters/httpapi/handler.go
+++ backend/internal/adapters/httpapi/handler.go
@@ -19,57 +19,67 @@ func NewHandler(shortURLService ports.ShortenerService) *Handler {
 }
 
 func (h *Handler) RegisterRoutes(router *http.ServeMux) {
-	router.HandleFunc("POST /", h.createShortURL)
-	router.HandleFunc("GET /{shortCode}", h.redirectFromShortCode)
+	router.Handle("POST /", h.handleCreateShortURL())
+	router.Handle("GET /{shortCode}", h.handleRedirectFromShortCode())
 }
 
-func (h *Handler) redirectFromShortCode(w http.ResponseWriter, r *http.Request) {
-	shortCode := r.PathValue("shortCode")
-
-	shortURL, err := h.shortURLService.Retrieve(r.Context(), shortCode)
-	if err != nil {
-		switch {
-		case errors.Is(err, domain.ErrShortURLNotFound):
-			w.WriteHeader(http.StatusNotFound)
-		default:
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-		return
-	}
-
-	http.Redirect(w, r, shortURL.TargetURL.String(), http.StatusTemporaryRedirect)
-}
-
-func (h *Handler) createShortURL(w http.ResponseWriter, r *http.Request) {
-	var request struct {
+func (h *Handler) handleCreateShortURL() http.Handler {
+	type requestDTO struct {
 		TargetURL string `json:"target_url"`
 	}
-
-	decoder := json.NewDecoder(r.Body)
-	decoder.DisallowUnknownFields()
-	if err := decoder.Decode(&request); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	targetURL, err := url.Parse(request.TargetURL)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	shortURL, err := h.shortURLService.Shorten(r.Context(), *targetURL)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	data := struct {
+	type responseDTO struct {
 		ShortCode string `json:"short_code"`
-	}{
-		ShortCode: shortURL.ShortCode,
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(data)
+
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			var request requestDTO
+			decoder := json.NewDecoder(r.Body)
+			decoder.DisallowUnknownFields()
+			if err := decoder.Decode(&request); err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			targetURL, err := url.Parse(request.TargetURL)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			shortURL, err := h.shortURLService.Shorten(r.Context(), *targetURL)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			data := responseDTO{
+				ShortCode: shortURL.ShortCode,
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			json.NewEncoder(w).Encode(data)
+		},
+	)
+}
+
+func (h *Handler) handleRedirectFromShortCode() http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			shortCode := r.PathValue("shortCode")
+
+			shortURL, err := h.shortURLService.Retrieve(r.Context(), shortCode)
+			if err != nil {
+				switch {
+				case errors.Is(err, domain.ErrShortURLNotFound):
+					w.WriteHeader(http.StatusNotFound)
+				default:
+					w.WriteHeader(http.StatusInternalServerError)
+				}
+				return
+			}
+
+			http.Redirect(w, r, shortURL.TargetURL.String(), http.StatusTemporaryRedirect)
+		},
+	)
 }
